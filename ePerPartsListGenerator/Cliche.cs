2e4b549b9709d23932b5da3165d917860b53dc0c
Chris <chris@csjt.co.uk>
2021-06-20 22:44:09 +0100

    Seperate cliche class into into its own file

---
+++ ePerPartsListGenerator/Cliche.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ePerPartsListGenerator
+{
+    class Cliche
+    {
+        public string PartNo;
+        public string Description; // The name of the parent part actually
+        public string ClicheCode;
+        public string ImagePath;
+        public List<Part> Parts;
+
+        public Cliche(string clicheCode)
+        {
+            ClicheCode = clicheCode;
+        }
+    }
+}
--- ePerPartsListGenerator/Drawing.cs
+++ ePerPartsListGenerator/Drawing.cs
@@ -2,19 +2,6 @@
 
 namespace ePerPartsListGenerator
 {
-    class Cliche
-    {
-        public string PartNo;
-        public string Description; // The name of the parent part actually
-        public string ClicheCode;
-        public string ImagePath;
-        public List<Part> Parts;
-
-        public Cliche(string clicheCode)
-        {
-            ClicheCode = clicheCode;
-        }
-    }
     class Drawing
     {
         public string Description;
